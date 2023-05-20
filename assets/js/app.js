e39946d2def7ced0aa87ba4e29d5d736ddf2c186
camilo <40795245+KROHOFF@users.noreply.github.com>
2023-05-19 21:31:33 -0400

    install-animate.css: instalacion de libreia de animaciones.css

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var logo = document.getElementById('logo');
+  
+    logo.addEventListener('click', function() {
+      this.classList.add('animate__animated');
+      this.classList.add('animate__rubberBand');
+      
+    });
+    return
+  });
